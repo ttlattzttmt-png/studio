58844867675c5e19afd4d3a39b136ade0657cf1b
hdgdb hdhdg <quality.mmmm@gmail.com>
2026-04-12 11:04:30 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'li

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -1,8 +1,7 @@
-
 'use client';
 
 import { useState } from 'react';
-import Link from 'link';
+import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { 
